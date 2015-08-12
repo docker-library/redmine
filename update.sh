8c60ec28849a82467178ecb646d397ec601a5c9a
Tianon Gravi <admwiggin@gmail.com>
2015-08-12 13:23:19 -0700

    Add initial .travis.yml

--- update.sh
+++ update.sh
@@ -15,6 +15,7 @@ versionsPage=$(curl -fsSL "$relasesUrl")
 passenger="$(curl -fsSL 'https://rubygems.org/api/v1/gems/passenger.json' | sed -r 's/^.*"version":"([^"]+)".*$/\1/')"
 ( set -x && sed -ri 's/^(ENV PASSENGER_VERSION) .*/\1 '"$passenger"'/;' Dockerfile*.template )
 
+travisEnv=
 for version in "${versions[@]}"; do
 	fullVersion="$(echo $versionsPage | sed -r "s/.*($version\.[0-9]+)\.tar\.gz[^.].*/\1/" | sort -V | tail -1)"
 	md5="$(curl -fsSL "$relasesUrl/redmine-$fullVersion.tar.gz.md5" | cut -d' ' -f1)"
@@ -28,5 +29,9 @@ for version in "${versions[@]}"; do
 		mkdir -p "$version/passenger"
 		sed 's/%%REDMINE%%/redmine:'"$version"'/' Dockerfile-passenger.template > "$version/passenger/Dockerfile"
 	)
+	
+	travisEnv='\n  - VERSION='"$version$travisEnv"
 done
 
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
