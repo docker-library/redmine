266e19aa01808ce16ba8ee98d443f5c059607b2a
Marc Wäckerlin <marc@waeckerlin.org>
2016-10-29 16:32:33 +0200

    move apt to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -8,11 +8,11 @@ ENV DEFAULT_LANGUAGE en
 RUN apt-get update
 RUN DEBIAN_FRONTEND=noninteractive apt-get install -y apache2 libapache2-mod-passenger bundler
 RUN DEBIAN_FRONTEND=noninteractive apt-get install -y emacs-nox debconf-utils
-RUN DEBIAN_FRONTEND=noninteractive apt-get install -dy redmine-mysql
+RUN DEBIAN_FRONTEND=noninteractive apt-get install -y redmine-mysql
+RUN chown -R www-data:www-data /usr/share/redmine
 RUN a2enmod passenger
 RUN rm -r /var/www/html
 RUN ln -s /usr/share/redmine/public /var/www/html
-RUN touch /firstrun
 ADD redmine.conf /etc/apache2/conf-available/redmine.conf
 RUN a2enconf redmine
 ADD start.sh /start.sh
--- start.sh
+++ start.sh
@@ -9,9 +9,9 @@ if test -z "$MYSQL_ENV_MYSQL_DATABASE" -o -z "$MYSQL_ENV_MYSQL_USER" -o -z "$MYS
     echo "            Redmine only supports MySQL 5.6, not 5.7" 1>&2
     exit 1
 fi
-if test -f /firstrun; then
-    mkdir -p /etc/redmine/default
-    cat > /etc/redmine/default/database.yml <<EOF
+
+mkdir -p /etc/redmine/default
+cat > /etc/redmine/default/database.yml <<EOF
 production:
   adapter: mysql
   database: ${MYSQL_ENV_MYSQL_DATABASE}
@@ -21,16 +21,6 @@ production:
   password: ${MYSQL_ENV_MYSQL_PASSWORD}
   encoding: utf8
 EOF
-    debconf-set-selections <<EOF
-redmine redmine/instances/default/dbconfig-install      boolean false
-redmine redmine/instances/default/dbconfig-reinstall    boolean false
-redmine redmine/instances/default/dbconfig-remove       boolean false
-redmine redmine/instances/default/dbconfig-upgrade      boolean false
-EOF
-    DEBIAN_FRONTEND=noninteractive apt-get install --no-download -y redmine-mysql
-    chown -R www-data:www-data /usr/share/redmine
-    rm /firstrun
-fi
 
 if test -f /run/apache2/apache2.pid; then
     rm /run/apache2/apache2.pid;
