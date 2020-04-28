174666753cabc7481a052330cf10d337c9fa54d0
Tianon Gravi <admwiggin@gmail.com>
2020-04-28 16:03:18 -0700

    Add initial GitHub Actions CI

--- update.sh
+++ update.sh
@@ -20,7 +20,6 @@ versionsPage="$(wget -qO- "$relasesUrl")"
 
 passenger="$(wget -qO- 'https://rubygems.org/api/v1/gems/passenger.json' | sed -r 's/^.*"version":"([^"]+)".*$/\1/')"
 
-travisEnv=
 for version in "${versions[@]}"; do
 	fullVersion="$(echo $versionsPage | sed -r "s/.*($version\.[0-9]+)\.tar\.gz[^.].*/\1/" | sort -V | tail -1)"
 	md5="$(wget -qO- "$relasesUrl/redmine-$fullVersion.tar.gz.md5" | cut -d' ' -f1)"
@@ -67,10 +66,4 @@ for version in "${versions[@]}"; do
 	cp docker-entrypoint.sh "$version/alpine/"
 	sed -i -e 's/gosu/su-exec/g' "$version/alpine/docker-entrypoint.sh"
 	sed "${commonSedArgs[@]}" "${alpineSedArgs[@]}" Dockerfile-alpine.template > "$version/alpine/Dockerfile"
-
-	travisEnv='\n  - VERSION='"$version/alpine$travisEnv"
-	travisEnv='\n  - VERSION='"$version$travisEnv"
 done
-
-travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
-echo "$travis" > .travis.yml
