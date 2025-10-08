d0061c1063ebc1e46ec4924ed010aca3e61ef47e
Tianon Gravi <admwiggin@gmail.com>
2025-10-08 14:56:15 -0700

    Moar review

--- 6.0/alpine3.21/docker-entrypoint.sh
+++ 6.0/alpine3.21/docker-entrypoint.sh
@@ -156,11 +156,13 @@ if [ -n "$isLikelyRedmine" ]; then
 		# empty string is truthy in ruby and so masks the generated fallback config
 		# https://github.com/rails/rails/blob/1aa9987169213ce5ce43c20b2643bc64c235e792/railties/lib/rails/application.rb#L454
 		unset SECRET_KEY_BASE
-	fi
-	# generate SECRET_KEY_BASE in-file if not set; this is not recommended unless the secret_token.rb is saved when container is recreated
-	if [ -z "${SECRET_KEY_BASE:-}" ] && [ ! -f config/initializers/secret_token.rb ]; then
-		echo >&2 'warning: no *SECRET_KEY_BASE set; running `rake generate_secret_token` to create one in "config/initializers/secret_token.rb"'
-		rake generate_secret_token
+		# generate SECRET_KEY_BASE in-file if not set; this is not recommended unless the secret_token.rb is saved when container is recreated
+		if [ ! -f config/initializers/secret_token.rb ]; then
+			echo >&2
+			echo >&2 'warning: no *SECRET_KEY_BASE set; running `rake generate_secret_token` to create one in "config/initializers/secret_token.rb"'
+			echo >&2
+			rake generate_secret_token
+		fi
 	fi
 
 	if [ "$1" != 'rake' -a -z "$REDMINE_NO_DB_MIGRATE" ]; then
